7605ba9533dc9baf1beecd6fb2841ccfc707d718
Nicoletapa <112115960+Nicoletapa@users.noreply.github.com>
2023-11-09 20:46:29 +0100

    Update CreateDB.sql

--- Nøsted/CreateDB.sql
+++ Nøsted/CreateDB.sql
@@ -1,116 +1,43 @@
-create database if not exists webAppDatabase;
-use webAppDatabase;
-create table AspNetRoleClaims
+-- Step 1: Create the database and use it
+CREATE DATABASE IF NOT EXISTS webAppDatabase2;
+USE webAppDatabase2;
+
+-- Step 2: Create tables without foreign key dependencies
+CREATE TABLE if not EXISTS AspNetRoles
 (
-    Id         int auto_increment
-        primary key,
-    ClaimType  varchar(255) not null,
-    ClaimValue varchar(255) not null,
-    RoleId     varchar(255) null,
-    constraint Id
-        unique (Id),
-    constraint aspnetroleclaims_ibfk_1
-        foreign key (RoleId) references webappdatabase.AspNetRoles (Id)
+    Id               VARCHAR(255) NOT NULL PRIMARY KEY,
+    Name             VARCHAR(255) NULL,
+    NormalizedName   VARCHAR(255) NULL,
+    ConcurrencyStamp VARCHAR(255) NULL
 );
 
-create index RoleId
-    on AspNetRoleClaims (RoleId);
-
-create table AspNetRoles
+CREATE TABLE if not EXISTS AspNetUsers
 (
-    Id               varchar(255) not null
-        primary key,
-    Name             varchar(255) null,
-    NormalizedName   varchar(255) null,
-    ConcurrencyStamp varchar(255) null
+    Id                   VARCHAR(255) NOT NULL PRIMARY KEY,
+    UserName             VARCHAR(255) NULL,
+    NormalizedUserName   VARCHAR(255) NULL,
+    Email                VARCHAR(255) NULL,
+    NormalizedEmail      VARCHAR(255) NULL,
+    EmailConfirmed       BIT NOT NULL,
+    PasswordHash         VARCHAR(255) NULL,
+    SecurityStamp        VARCHAR(255) NULL,
+    ConcurrencyStamp     VARCHAR(255) NULL,
+    PhoneNumber          VARCHAR(50) NULL,
+    PhoneNumberConfirmed BIT NOT NULL,
+    TwoFactorEnabled     BIT NOT NULL,
+    LockoutEnd           TIMESTAMP NULL,
+    LockoutEnabled       BIT NOT NULL,
+    AccessFailedCount    INT NOT NULL,
+    Discriminator        VARCHAR(255) NULL
 );
 
-create table AspNetUserClaims
+CREATE TABLE if not EXISTS Kategori
 (
-    Id         int auto_increment
-        primary key,
-    ClaimType  varchar(255) null,
-    ClaimValue varchar(255) null,
-    UserId     varchar(255) null,
-    constraint Id
-        unique (Id),
-    constraint aspnetuserclaims_ibfk_1
-        foreign key (UserId) references webappdatabase.AspNetUsers (Id)
+    KategoriID   INT NOT NULL PRIMARY KEY,
+    KategoriNavn VARCHAR(50) NULL
 );
 
-create index UserId
-    on AspNetUserClaims (UserId);
-
-create table AspNetUserLogins
-(
-    LoginProvider       int auto_increment
-        primary key,
-    ProviderKey         varchar(255) not null,
-    ProviderDisplayName varchar(255) not null,
-    UserId              varchar(255) not null,
-    constraint LoginProvider
-        unique (LoginProvider),
-    constraint aspnetuserlogins_ibfk_1
-        foreign key (UserId) references webappdatabase.AspNetUsers (Id)
-);
-
-create index UserId
-    on AspNetUserLogins (UserId);
-
-create table AspNetUserRoles
-(
-    UserId varchar(255) not null,
-    RoleId varchar(255) not null,
-    primary key (UserId, RoleId),
-    constraint aspnetuserroles_ibfk_1
-        foreign key (UserId) references webappdatabase.AspNetUsers (Id),
-    constraint aspnetuserroles_ibfk_2
-        foreign key (RoleId) references webappdatabase.AspNetRoles (Id)
-);
-
-create index RoleId
-    on AspNetUserRoles (RoleId);
-
-create table AspNetUserTokens
-(
-    UserId        varchar(255) not null,
-    LoginProvider varchar(255) not null,
-    Name          varchar(255) not null,
-    Value         varchar(255) null,
-    primary key (UserId, LoginProvider)
-);
-
-create table AspNetUsers
-(
-    Id                   varchar(255) not null
-        primary key,
-    UserName             varchar(255) null,
-    NormalizedUserName   varchar(255) null,
-    Email                varchar(255) null,
-    NormalizedEmail      varchar(255) null,
-    EmailConfirmed       bit          not null,
-    PasswordHash         varchar(255) null,
-    SecurityStamp        varchar(255) null,
-    ConcurrencyStamp     varchar(255) null,
-    PhoneNumber          varchar(50)  null,
-    PhoneNumberConfirmed bit          not null,
-    TwoFactorEnabled     bit          not null,
-    LockoutEnd           timestamp    null,
-    LockoutEnabled       bit          not null,
-    AccessFailedCount    int          not null,
-    Discriminator        varchar(255) null,
-    constraint Id
-        unique (Id)
-);
-
-create table Kategori
-(
-    KategoriID   int         not null
-        primary key,
-    KategoriNavn varchar(50) null
-);
-
-create table Ordre1
+CREATE TABLE if not EXISTS Ordre1
 (
     OrdreNr               int auto_increment
         primary key,
@@ -131,30 +58,97 @@ create table Ordre1
     Status                tinyint(1)     null
 );
 
-create table Sjekkliste
+CREATE TABLE if not EXISTS __EFMigrationsHistory
 (
-    SjekklisteID int auto_increment
-        primary key,
-    OrdreNr      int not null,
-    constraint Sjekkliste___fk
-        foreign key (OrdreNr) references webappdatabase2.Ordre1 (OrdreNr)
+    MigrationId    VARCHAR(150) NOT NULL PRIMARY KEY,
+    ProductVersion VARCHAR(32) NOT NULL
 );
 
-create table SjekklisteSjekkpunkt
+CREATE TABLE if not EXISTS bruker
 (
-    SjekklisteSjekkpunktID int          not null
+    Id    INT AUTO_INCREMENT PRIMARY KEY,
+    Name  VARCHAR(255) NULL,
+    Email VARCHAR(255) NULL,
+    CONSTRAINT Email UNIQUE (Email)
+);
+
+-- Step 3: Create tables with foreign key dependencies
+CREATE TABLE if not EXISTS AspNetRoleClaims
+(
+    Id         int auto_increment
         primary key,
-    SjekklisteID           int          null,
-    Status                 varchar(255) null,
-    SjekkpunktID           int          not null,
-    constraint SjekklisteSjekkpunkt___fk
-        foreign key (SjekklisteID) references webappdatabase2.Sjekkliste (SjekklisteID),
-    constraint sjekklistesjekkpunkt_ibfk_2
-        foreign key (SjekkpunktID) references webappdatabase2.Sjekkpunkt2 (SjekkpunktID)
+    ClaimType  varchar(255) not null,
+    ClaimValue varchar(255) not null,
+    RoleId     varchar(255) null,
+    constraint Id
+        unique (Id),
+    constraint aspnetroleclaims_ibfk_1
+        foreign key (RoleId) references webappdatabase.AspNetRoles (Id)
+);
+
+CREATE INDEX RoleId ON AspNetRoleClaims (RoleId);
+
+CREATE TABLE if not EXISTS AspNetUserClaims
+(
+    Id         int auto_increment
+        primary key,
+    ClaimType  varchar(255) null,
+    ClaimValue varchar(255) null,
+    UserId     varchar(255) null,
+    constraint Id
+        unique (Id),
+    constraint aspnetuserclaims_ibfk_1
+        foreign key (UserId) references webappdatabase.AspNetUsers (Id)
+);
+
+CREATE INDEX UserId ON AspNetUserClaims (UserId);
+
+CREATE TABLE if not EXISTS AspNetUserLogins
+(
+    LoginProvider       int auto_increment
+        primary key,
+    ProviderKey         varchar(255) not null,
+    ProviderDisplayName varchar(255) not null,
+    UserId              varchar(255) not null,
+    constraint LoginProvider
+        unique (LoginProvider),
+    constraint aspnetuserlogins_ibfk_1
+        foreign key (UserId) references webappdatabase.AspNetUsers (Id)
+);
+
+CREATE INDEX UserId ON AspNetUserLogins (UserId);
+
+CREATE TABLE if not EXISTS AspNetUserRoles
+(
+    UserId varchar(255) not null,
+    RoleId varchar(255) not null,
+    primary key (UserId, RoleId),
+    constraint aspnetuserroles_ibfk_1
+        foreign key (UserId) references webappdatabase.AspNetUsers (Id),
+    constraint aspnetuserroles_ibfk_2
+        foreign key (RoleId) references webappdatabase.AspNetRoles (Id)
+);
+
+CREATE INDEX RoleId ON AspNetUserRoles (RoleId);
+
+CREATE TABLE if not EXISTS AspNetUserTokens
+(
+    UserId        varchar(255) not null,
+    LoginProvider varchar(255) not null,
+    Name          varchar(255) not null,
+    Value         varchar(255) null,
+    primary key (UserId, LoginProvider)
+);
+
+CREATE TABLE IF NOT EXISTS Sjekkliste
+(
+    SjekklisteID INT AUTO_INCREMENT PRIMARY KEY,
+    OrdreNr      INT NOT NULL,
+    CONSTRAINT Sjekkliste___fk FOREIGN KEY (OrdreNr) REFERENCES Ordre1 (OrdreNr)
 );
 
 
-create table Sjekkpunkt2
+CREATE TABLE if not EXISTS Sjekkpunkt2
 (
     SjekkpunktID   int          not null
         primary key,
@@ -167,25 +161,43 @@ create table Sjekkpunkt2
         foreign key (KategoriID) references webappdatabase2.Kategori (KategoriID)
 );
 
-create index KategoriID
-    on Sjekkpunkt2 (KategoriID);
+CREATE INDEX KategoriID ON Sjekkpunkt2 (KategoriID);
 
-create table __EFMigrationsHistory
+CREATE TABLE if not EXISTS SjekklisteSjekkpunkt
 (
-    MigrationId    varchar(150) not null
+    SjekklisteSjekkpunktID int          not null
         primary key,
-    ProductVersion varchar(32)  not null
+    SjekklisteID           int          null,
+    Status                 varchar(255) null,
+    SjekkpunktID           int          not null,
+    constraint SjekklisteSjekkpunkt___fk
+        foreign key (SjekklisteID) references webappdatabase2.Sjekkliste (SjekklisteID),
+    constraint sjekklistesjekkpunkt_ibfk_2
+        foreign key (SjekkpunktID) references webappdatabase2.Sjekkpunkt2 (SjekkpunktID)
 );
 
-create table bruker
-(
-    Id    int auto_increment
-        primary key,
-    Name  varchar(255) null,
-    Email varchar(255) null,
-    constraint Email
-        unique (Email),
-    constraint Id
-        unique (Id)
-);
+INSERT INTO Sjekkpunkt2 (SjekkpunktID, SjekkpunktNavn, KategoriID) VALUES
+(1, 'Sjekk clutch lameller for slitasje', 1),
+(2, 'Sjekk bremser. Bånd/pål', 1),
+(3, 'Sjekk lager for trommel', 1),
+(4, 'Sjekk PTO og opplagring', 1),
+(5, 'Sjekk kjede strammer', 1),
+(6, 'Sjekk wire', 1),
+(7, 'Sjekk pinion lager', 1),
+(8, 'Sjekk kile på kjedehjul', 1),
+(9, 'Sjekk hydraulikk sylinder for lekkasje', 2),
+(10, 'Sjekk kile på kjedehjul', 2),
+(11, 'Test hydraulikk blokk i testbenk', 2),
+(12, 'Skift olje i tank', 2),
+(13, 'Skift olje på gir boks', 2),
+(14, 'Sjekk ringsylinder åpne og skift tetninger', 2),
+(15, 'Sjekk bremse sylinder åpne og skift tetninger', 2),
+(16, 'Sjekk ledningsnett på vinsj', 3),
+(17, 'Sjekk og test radio', 3),
+(18, 'Sjekk og test knappekasse', 3),
+(19, 'Xx_bar', 4),
+(20, 'Test vinsj og kjør alle funksjoner', 4),
+(21, 'TrekkKraft KN', 5),
+(22, 'BremseKraft KN', 5),
+(23, 'Kommentar', 5);
 
